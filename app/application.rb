a96704c548706fe9bef72af4df587f5946cd1054
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-06-28 21:02:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,13 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+    resp.write "#{item.price}\n"
+      end
+    else
+      resp.write "Route not found"
   end
 
 end
